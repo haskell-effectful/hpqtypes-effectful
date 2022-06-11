2d14abc14b6aea5e40eb845b596391df0b1d506c
Jakub Waszczuk <jakub.waszczuk@scrive.com>
2022-06-11 18:28:42 +0200

    Playing around

--- lib/MyLib.hs
+++ lib/MyLib.hs
@@ -1,4 +1,48 @@
-module MyLib (someFunc) where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+module MyLib
+  ( EffectDB
+  ) where
+
+
+import Effectful
+import Database.PostgreSQL.PQTypes.SQL.Class (IsSQL)
+import Database.PostgreSQL.PQTypes (SomeSQL)
+import Database.PostgreSQL.PQTypes.Internal.Connection (ConnectionStats)
+import qualified Database.PostgreSQL.PQTypes.SQL.Class as PQ
+import qualified Database.PostgreSQL.PQTypes.Class as PQ
+import Database.PostgreSQL.PQTypes.Internal.Monad (runDBT)
+
+import Database.PostgreSQL.PQTypes.Internal.QueryResult (QueryResult)
+
+import Effectful.Dispatch.Dynamic
+
+
+
+data EffectDB :: Effect where
+  RunQuery :: IsSQL sql => sql -> EffectDB m Int
+  -- RunPreparedQuery :: IsSQL sql => PQ.QueryName -> sql -> EffectDB m Int
+  -- GetLastQuery :: EffectDB m SomeSQL
+  WithFrozenLastQuery :: EffectDB m a -> EffectDB m a
+
+
+type instance DispatchOf EffectDB = 'Dynamic
+
+
+runEffectDB
+  :: (IOE :> es)
+  => Eff (EffectDB : es) a
+  -> Eff es a
+runEffectDB = interpret $ \_ -> \case
+  RunQuery sql -> liftIO $
+    runDBT undefined undefined (PQ.runQuery sql)
+  WithFrozenLastQuery dbEff ->
+    undefined -- PQ.withFrozenLastQuery dbEff
