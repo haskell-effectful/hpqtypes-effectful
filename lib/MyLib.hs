1ad2065c90edc4ab7c708e93ed369032c728c1c0
Jakub Waszczuk <jakub.waszczuk@scrive.com>
2022-06-12 11:46:51 +0200

    Playing around continued

--- lib/MyLib.hs
+++ lib/MyLib.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 
 
 module MyLib
@@ -19,30 +20,53 @@ import Database.PostgreSQL.PQTypes (SomeSQL)
 import Database.PostgreSQL.PQTypes.Internal.Connection (ConnectionStats)
 import qualified Database.PostgreSQL.PQTypes.SQL.Class as PQ
 import qualified Database.PostgreSQL.PQTypes.Class as PQ
-import Database.PostgreSQL.PQTypes.Internal.Monad (runDBT)
+import Database.PostgreSQL.PQTypes.Internal.Monad (runDBT, DBT)
 
 import Database.PostgreSQL.PQTypes.Internal.QueryResult (QueryResult)
 
 import Effectful.Dispatch.Dynamic
 
 
+-- data DBE :: Effect where
+--   DBE :: DBT IO a -> DBE m a
+
+
+-- type instance DispatchOf DBE = 'Dynamic
+
+
+-- runDBE :: (IOE :> es) => Eff (DBE : es) a -> Eff es a
+-- runDBE = undefined
+
 
 data EffectDB :: Effect where
   RunQuery :: IsSQL sql => sql -> EffectDB m Int
   -- RunPreparedQuery :: IsSQL sql => PQ.QueryName -> sql -> EffectDB m Int
   -- GetLastQuery :: EffectDB m SomeSQL
-  WithFrozenLastQuery :: EffectDB m a -> EffectDB m a
+  WithFrozenLastQuery :: m a -> EffectDB m a
 
 
 type instance DispatchOf EffectDB = 'Dynamic
 
 
 runEffectDB
-  :: (IOE :> es)
+  :: forall es a. (IOE :> es)
   => Eff (EffectDB : es) a
   -> Eff es a
-runEffectDB = interpret $ \_ -> \case
-  RunQuery sql -> liftIO $
-    runDBT undefined undefined (PQ.runQuery sql)
-  WithFrozenLastQuery dbEff ->
-    undefined -- PQ.withFrozenLastQuery dbEff
+runEffectDB = interpret $ \env -> \case
+  RunQuery sql -> liftIO .
+    runDBT undefined undefined $ PQ.runQuery sql
+  WithFrozenLastQuery (action :: Eff localEs b) -> do
+    -- localSeqUnliftIO env $ \unlift -> unlift action
+    localSeqUnliftIO env $ \unlift -> (unlift action :: IO b)
+    -- liftIO $ runDBT undefined undefined $ PQ.withFrozenLastQuery result
+
+
+-- runEffectDB
+--   :: forall es a. (IOE :> es)
+--   => Eff (EffectDB : es) a
+--   -> Eff es a
+-- runEffectDB = interpret $ \env -> \case
+--   RunQuery sql -> send . DBE $ PQ.runQuery sql
+--   WithFrozenLastQuery (action :: Eff localEs b) -> do
+--     localSeqUnlift env $ \unlift -> PQ.withFrozenLastQuery (unlift action :: Eff es b)
+--     -- liftIO $ runDBT undefined undefined $ PQ.withFrozenLastQuery result
