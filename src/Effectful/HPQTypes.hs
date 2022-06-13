8620acf017511e85282c383e8b02f25802e54d7a
Kyriakos Papachrysanthou <k.papachrisanthou@gmail.com>
2022-06-13 12:07:54 +0200

    Change module name and src folder

--- src/Effectful/HPQTypes.hs
+++ src/Effectful/HPQTypes.hs
@@ -5,11 +5,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 
-module MyLib
+module Effectful.HPQTypes
   ( EffectDB (..)
   , runQuery
   , getQueryResult
@@ -18,15 +17,12 @@ module MyLib
   , foldlDB
   , fetchMany
   , runEffectDB
-  , main
   )
 where
 
 import Control.Concurrent.MVar
 import Control.Monad.Base (liftBase)
 import qualified Data.Foldable as F
-import Data.Int (Int32)
-import qualified Data.Text as T
 import qualified Database.PostgreSQL.PQTypes as PQ
 import qualified Database.PostgreSQL.PQTypes.Internal.Connection as PQ
 import qualified Database.PostgreSQL.PQTypes.Internal.Query as PQ
@@ -35,7 +31,6 @@ import Effectful
 import Effectful.Dispatch.Dynamic
 import Effectful.Error.Static
 import Effectful.State.Static.Local
-import System.Environment (getEnv)
 
 data EffectDB :: Effect where
   RunQuery :: PQ.IsSQL sql => sql -> EffectDB m Int
@@ -113,19 +108,3 @@ runEffectDB connectionSource transactionSettings =
         , PQ.dbRecordLastQuery = True
         , PQ.dbQueryResult = Nothing
         }
-
-main :: IO ()
-main = do
-  dbUrl <- T.pack <$> getEnv "DATABASE_URL"
-  let connectionSource = PQ.unConnectionSource $ PQ.simpleSource $ PQ.ConnectionSettings dbUrl Nothing []
-      transactionSettings = PQ.defaultTransactionSettings
-      sql :: PQ.SQL = PQ.mkSQL "SELECT 1"
-      program :: Eff '[EffectDB, Error PQ.HPQTypesError, IOE] ()
-      program = do
-        rowNo <- runQuery sql
-        liftBase $ putStr "Row number: " >> print rowNo
-        queryResult :: [Int32] <- fetchMany PQ.runIdentity
-        liftBase $ putStr "Result(s): " >> print queryResult
-        connectionStats <- send $ GetConnectionStats
-        liftBase $ putStr "Connection stats: " >> print connectionStats
-  (runEff . runErrorNoCallStack @PQ.HPQTypesError $ runEffectDB connectionSource transactionSettings program) >>= print
