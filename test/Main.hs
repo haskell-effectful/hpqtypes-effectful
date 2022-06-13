dd59f4398b7a7f037cfeb16db63097acf6a25a80
Jakub Janczak <jakub.janczak@scrive.com>
2022-06-13 17:13:58 +0200

    Styling

--- test/Main.hs
+++ test/Main.hs
@@ -5,17 +5,17 @@
 
 module Main (main) where
 
-import Database.PostgreSQL.PQTypes.SQL.Class
 import Control.Exception (assert)
 import Control.Monad (void)
 import Control.Monad.Base (liftBase)
 import Data.Int (Int32)
 import qualified Data.Text as T
 import qualified Database.PostgreSQL.PQTypes as PQ
+import Database.PostgreSQL.PQTypes.SQL.Class
 import Effectful
+import Effectful.Dispatch.Dynamic
 import Effectful.Error.Static
 import Effectful.HPQTypes
-import Effectful.Dispatch.Dynamic
 import System.Environment (getEnv)
 
 main :: IO ()
@@ -31,15 +31,15 @@ main = do
 
         queryResult :: [Int32] <- fetchMany PQ.runIdentity
         liftBase $ putStr "Result(s): " >> print queryResult
-        
+
         connectionStats <- getConnectionStats
         liftBase $ putStr "Connection stats: " >> print connectionStats
 
-        (SomeSQL lq) <- send $ GetLastQuery 
+        (SomeSQL lq) <- send $ GetLastQuery
         withFrozenLastQuery $ do
           let newSQL = "SELECT 2"
           void . runQuery $ PQ.mkSQL newSQL
-          (SomeSQL newLq) <- send $ GetLastQuery 
+          (SomeSQL newLq) <- send $ GetLastQuery
           void . return $ assert ((T.pack $ show newLq) == newSQL)
         void . return $ assert ((T.pack $ show lq) == sql)
   (runEff . runErrorNoCallStack @PQ.HPQTypesError $ runEffectDB connectionSource transactionSettings program) >>= print
