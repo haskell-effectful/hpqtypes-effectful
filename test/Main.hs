539368e1c05696c3844ac77b54323504a879ea3a
Jakub Janczak <kubek2k@gmail.com>
2022-06-13 23:03:35 +0200

    Testing withFrozenLastQuery and getLastQuery (#16)
    * Testing withFrozenLastQuery and getLastQuery

--- test/Main.hs
+++ test/Main.hs
@@ -10,8 +10,8 @@ import Control.Monad.Base (liftBase)
 import Data.Int (Int32)
 import qualified Data.Text as T
 import qualified Database.PostgreSQL.PQTypes as PQ
+import Database.PostgreSQL.PQTypes.SQL.Class
 import Effectful
-import Effectful.Dispatch.Dynamic
 import Effectful.Error.Static
 import Effectful.HPQTypes
 import System.Environment (getEnv)
@@ -32,13 +32,23 @@ testPrintConnectionStats = do
   dbUrl <- T.pack <$> getEnv "DATABASE_URL"
   let connectionSource = PQ.unConnectionSource $ PQ.simpleSource $ PQ.ConnectionSettings dbUrl Nothing []
       transactionSettings = PQ.defaultTransactionSettings
-      sql :: PQ.SQL = PQ.mkSQL "SELECT 1"
+      sql = "SELECT 1"
       program :: Eff '[EffectDB, Error PQ.HPQTypesError, IOE] ()
       program = do
-        rowNo <- runQuery sql
+        rowNo <- runQuery $ PQ.mkSQL sql
         liftBase $ putStr "Row number: " >> print rowNo
+
         queryResult :: [Int32] <- fetchMany PQ.runIdentity
         liftBase $ putStr "Result(s): " >> print queryResult
-        connectionStats <- send GetConnectionStats
+
+        connectionStats <- getConnectionStats
         liftBase $ putStr "Connection stats: " >> print connectionStats
-  void $ (runEff . runErrorNoCallStack @PQ.HPQTypesError $ runEffectDB connectionSource transactionSettings program) >>= print
+
+        (SomeSQL lq) <- getLastQuery
+        withFrozenLastQuery $ do
+          let newSQL = "SELECT 2"
+          void . runQuery $ PQ.mkSQL newSQL
+          (SomeSQL newLq) <- getLastQuery
+          liftIO $ assertEqual "SQL don't match" (show newLq) (show $ PQ.mkSQL sql)
+        liftIO $ assertEqual "SQL don't match" (show lq) (show $ PQ.mkSQL sql)
+  (runEff . runErrorNoCallStack @PQ.HPQTypesError $ runEffectDB connectionSource transactionSettings program) >>= print
