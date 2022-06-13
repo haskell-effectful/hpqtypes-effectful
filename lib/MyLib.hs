f073d6846f92751bbc8fa0fdb066470469dfc3a2
Kyriakos Papachrysanthou <k.papachrisanthou@gmail.com>
2022-06-13 11:50:56 +0200

    Add formatting (#7)

--- lib/MyLib.hs
+++ lib/MyLib.hs
@@ -1,14 +1,13 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
-
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 
 module MyLib
   ( EffectDB (..)
@@ -20,27 +19,23 @@ module MyLib
   , fetchMany
   , runEffectDB
   , main
-  ) where
+  )
+where
 
-
-import Control.Monad.Base (liftBase)
-import Data.Int (Int32)
-import qualified Data.Foldable as F
-
-import Effectful
-import Effectful.Dispatch.Dynamic
-import Effectful.State.Static.Local
-import Effectful.Error.Static
 import Control.Concurrent.MVar
-import System.Environment (getEnv)
-
+import Control.Monad.Base (liftBase)
+import qualified Data.Foldable as F
+import Data.Int (Int32)
 import qualified Data.Text as T
 import qualified Database.PostgreSQL.PQTypes as PQ
 import qualified Database.PostgreSQL.PQTypes.Internal.Connection as PQ
-import qualified Database.PostgreSQL.PQTypes.Internal.State as PQ
 import qualified Database.PostgreSQL.PQTypes.Internal.Query as PQ
-
-
+import qualified Database.PostgreSQL.PQTypes.Internal.State as PQ
+import Effectful
+import Effectful.Dispatch.Dynamic
+import Effectful.Error.Static
+import Effectful.State.Static.Local
+import System.Environment (getEnv)
 
 data EffectDB :: Effect where
   RunQuery :: PQ.IsSQL sql => sql -> EffectDB m Int
@@ -50,81 +45,74 @@ data EffectDB :: Effect where
   -- GetLastQuery :: EffectDB m SomeSQL
   WithFrozenLastQuery :: m a -> EffectDB m a
 
-
 type instance DispatchOf EffectDB = 'Dynamic
-
 
 runQuery :: (EffectDB :> es, PQ.IsSQL sql) => sql -> Eff es Int
 runQuery = send . RunQuery
 
-
 getQueryResult :: (EffectDB :> es, PQ.FromRow row) => Eff es (Maybe (PQ.QueryResult row))
 getQueryResult = send GetQueryResult
-
 
 withFrozenLastQuery :: (EffectDB :> es) => Eff es a -> Eff es a
 withFrozenLastQuery = send . WithFrozenLastQuery
 
-
-{-# INLINABLE foldrDB #-}
+{-# INLINEABLE foldrDB #-}
 foldrDB :: (PQ.FromRow row, EffectDB :> es) => (row -> acc -> Eff es acc) -> acc -> Eff es acc
 foldrDB f acc = maybe (return acc) (F.foldrM f acc) =<< getQueryResult
 
-
-{-# INLINABLE foldlDB #-}
+{-# INLINEABLE foldlDB #-}
 foldlDB :: (PQ.FromRow row, EffectDB :> es) => (acc -> row -> Eff es acc) -> acc -> Eff es acc
 foldlDB f acc = maybe (return acc) (F.foldlM f acc) =<< getQueryResult
 
-
-{-# INLINABLE fetchMany #-}
+{-# INLINEABLE fetchMany #-}
 fetchMany :: (PQ.FromRow row, EffectDB :> es) => (row -> t) -> Eff es [t]
 fetchMany f = foldrDB (\row acc -> return $ f row : acc) []
 
-
-runEffectDB
-  :: forall es a. (IOE :> es, Error PQ.HPQTypesError :> es)
-  => PQ.ConnectionSourceM (Eff es)
-  -> PQ.TransactionSettings
-  -> Eff (EffectDB : es) a
-  -> Eff es a
+runEffectDB ::
+  forall es a.
+  (IOE :> es, Error PQ.HPQTypesError :> es) =>
+  PQ.ConnectionSourceM (Eff es) ->
+  PQ.TransactionSettings ->
+  Eff (EffectDB : es) a ->
+  Eff es a
 runEffectDB connectionSource transactionSettings =
-    reinterpret runWithState $ \env -> \case
-      RunQuery sql -> do
-        dbState <- get
-        (result, dbState') <- liftBase $ PQ.runQueryIO sql (dbState :: PQ.DBState (Eff es))
-        put dbState'
-        pure result
-      GetQueryResult -> do
-        dbState :: PQ.DBState (Eff es) <- get
-        pure $ PQ.dbQueryResult dbState
-      WithFrozenLastQuery (action :: Eff localEs b) -> do
-        st :: PQ.DBState (Eff es) <- get
-        put st { PQ.dbRecordLastQuery = False }
-        result <- localSeqUnlift env $ \unlift -> unlift action
-        modify $ \(st' :: PQ.DBState (Eff es)) ->
-          st' { PQ.dbRecordLastQuery = PQ.dbRecordLastQuery st }
-        pure result
-      GetConnectionStats -> do
-        dbState :: PQ.DBState (Eff es) <- get
-        mconn <- liftIO . readMVar . PQ.unConnection $ PQ.dbConnection dbState 
-        case mconn of
-          Nothing -> throwError $ PQ.HPQTypesError "getConnectionStats: no connection"
-          Just cd -> return $ PQ.cdStats cd
+  reinterpret runWithState $ \env -> \case
+    RunQuery sql -> do
+      dbState <- get
+      (result, dbState') <- liftBase $ PQ.runQueryIO sql (dbState :: PQ.DBState (Eff es))
+      put dbState'
+      pure result
+    GetQueryResult -> do
+      dbState :: PQ.DBState (Eff es) <- get
+      pure $ PQ.dbQueryResult dbState
+    WithFrozenLastQuery (action :: Eff localEs b) -> do
+      st :: PQ.DBState (Eff es) <- get
+      put st {PQ.dbRecordLastQuery = False}
+      result <- localSeqUnlift env $ \unlift -> unlift action
+      modify $ \(st' :: PQ.DBState (Eff es)) ->
+        st' {PQ.dbRecordLastQuery = PQ.dbRecordLastQuery st}
+      pure result
+    GetConnectionStats -> do
+      dbState :: PQ.DBState (Eff es) <- get
+      mconn <- liftIO . readMVar . PQ.unConnection $ PQ.dbConnection dbState
+      case mconn of
+        Nothing -> throwError $ PQ.HPQTypesError "getConnectionStats: no connection"
+        Just cd -> return $ PQ.cdStats cd
   where
     runWithState :: Eff (State (PQ.DBState (Eff es)) : es) a -> Eff es a
     runWithState eff =
       PQ.withConnection connectionSource $ \conn -> do
         let dbState0 = mkDBConn conn
         evalState dbState0 eff :: Eff es a
-    mkDBConn conn = PQ.DBState
-      { PQ.dbConnection = conn
-      , PQ.dbConnectionSource = connectionSource
-      , PQ.dbTransactionSettings = transactionSettings
-      , PQ.dbLastQuery = PQ.SomeSQL (mempty :: PQ.SQL)
-      , PQ.dbRecordLastQuery = True
-      , PQ.dbQueryResult = Nothing
-      }
-
+    mkDBConn conn =
+      PQ.DBState
+        { PQ.dbConnection = conn
+        , PQ.dbConnectionSource = connectionSource
+        , PQ.dbTransactionSettings = transactionSettings
+        , PQ.dbLastQuery = PQ.SomeSQL (mempty :: PQ.SQL)
+        , PQ.dbRecordLastQuery = True
+        , PQ.dbQueryResult = Nothing
+        }
 
 main :: IO ()
 main = do
